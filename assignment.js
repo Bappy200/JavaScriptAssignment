84c0ca59b49321d4334c26f83458ac88cd46cf8e
Sajjadul <sbappyi200@gmail.com>
2021-01-22 11:20:44 +0600

    complite hotelCost and megaFriend

--- assignment.js
+++ assignment.js
@@ -23,7 +23,64 @@ function budgetCalculator(watch, phone, laptop){
 }
 
 
-//
-var budget = budgetCalculator(1,2,-2);
+//Calculate the total hotel cost
+function hotelCost(day){
+    if(day>0 && typeof day == 'number'){
+        var totalCost = 0;
+        if(day <=10){
+            totalCost = day * 100;
+        }
+        else if(day<=20){
+            var firstPart = 10 * 100;
+            var remaining = day - 10;
+            var secondPart = remaining * 80;
+            totalCost = firstPart + secondPart; 
+        }
+        else{
+            var firstPart = 10 * 100;
+            var secondPart = 10 * 80;
+            var remaining = day - 20;
+            var lastPart = remaining * 50;
+            totalCost = firstPart + secondPart + lastPart;
+        }
+        return totalCost;
+    }
+    else{
+        return 'Please input a positive number'; 
+    }   
+}
+
+
+//Get the large length name
+function megaFriend(names){
+    if(names.length > 0 && Array.isArray(names)){
+        var largeName = names[0];
+        var largeLength = largeName.length;
+        for(var i = 1; i < names.length; i++){
+            var currentLength = names[i].length;
+            if(currentLength > largeLength){
+                largeName = names[i];
+                largeLength = currentLength;
+            }
+        }
+        return largeName;
+    }
+    else{
+        return 'Please input right name.'
+    }
+}
+
+
+// call function
+var meter = kilometerToMeter(4.4);
+console.log(meter);
+
+var budget = budgetCalculator(2,3,4);
 console.log(budget);
+
+var totalHotelCost = hotelCost(22);
+console.log(totalHotelCost);
+
+var largeName = megaFriend(['sajjaudl','rabby','shaharin islam','jewel islam']);
+console.log(largeName);
 
